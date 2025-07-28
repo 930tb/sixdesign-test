c47ac354af0cbe342137d4443ed6c616e6636d0c
930tb <sixdesign.g@gmail.com>
2025-07-28 12:11:17 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// JavaScript
+  /* JavaScript */
     function toggleMenu() {
     const menu = document.getElementById('menu');
     const trigger = document.querySelector('.hamburger');
-    const logoContainer = document.querySelector('.logo-container-1'); // ロゴコンテナを取得
+    const logoContainer = document.querySelector('.logo-container-1'); /* ロゴコンテナを取得 */
 
     menu.classList.toggle('open');
     trigger.classList.toggle('open');
 
-    // ロゴコンテナの表示/非表示を切り替える
+    /* ロゴコンテナの表示/非表示を切り替える */
     logoContainer.classList.toggle('hidden');
   }
