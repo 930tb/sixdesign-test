558b785f59443773222b8db1294952aa58f5fabb
930tb <sixdesign.g@gmail.com>
2025-08-02 00:01:26 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
     function toggleMenu() {
     const menu = document.getElementById('menu');
     const trigger = document.querySelector('.hamburger');
-    const logoContainer = document.querySelector('.logo-container-1'); /* ロゴコンテナを取得 */
+    const headerLogo = document.querySelector('.logo'); /* ロゴを取得 */
+    const headerColor = document.querySelector('.header-color'); /* header-colorを取得 */
 
     menu.classList.toggle('open');
     trigger.classList.toggle('open');
 
-    /* ロゴコンテナの表示/非表示を切り替える */
-    logoContainer.classList.toggle('hidden');
+    /* ロゴコンテナとheader-colorの表示/非表示を切り替える */
+    headerLogo.classList.toggle('hidden');
+    headerColor.classList.toggle('hidden');
   }
